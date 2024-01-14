8805e76bdc4a199cefd868a8c21d6282cf362a11
Mark Leighton <markconnorleighton@gmail.com>
2024-01-14 15:35:28 +0000

    Add project files.

---
+++ AoCDay1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TreeBucket
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllText(@"C:\Users\Mark\source\repos\AoCDay1\AoCDay1\input.txt");
+            string[] strings = Regex.Replace(input, @"[A-Za-z]+", "").Split('\n');
+            int sum = 0;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                strings[i] = strings[i].Trim();
+                string output = "";
+                output += strings[i][0];
+                output += strings[i][^1];
+                sum += Int32.Parse(output);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
